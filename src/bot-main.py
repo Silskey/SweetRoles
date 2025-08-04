b0457d3e69a6e95adfc5b4481870a9bb0c4d0e2b
Silskey <silskeyos@gmail.com>
2025-08-04 22:03:42 +0100

    added first initial commit with add role command. setup command soon

---
+++ src/bot-main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+TOKEN = 'MTQwMjAwNTQ5NzEwMzc3NzgxMg.GWA3e9.tXph7VgmwXNAwbz71K2F8u6BO0sZZv-l98P1sI'
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True  
+bot = commands.Bot(intents=intents)
+discord.Permissions.all()
+guildslist = bot.guilds
+
+@bot.event
+async def on_ready():
+    print(f"Bot is logged in as {bot.user}")
+
+
+@bot.slash_command(guild=guildslist, name="giverole",description="assigns a role to a given user")
+@discord.option("user",type=discord.SlashCommandOptionType.user,required=True,description="the user to assign the role to")
+@discord.option("role",type=discord.SlashCommandOptionType.role,required=True,description="the role to give the user")
+async def giverole(ctx,user,role):
+    await ctx.respond(f"trying to assign {role} to {user}")
+    await user.add_roles(role,reason=f"{ctx.author} command call to add {role} to {user}")
+    return 0
+
+def get_similar_guilds(user):
+    guilds =[]
+    for guild in bot.guilds:
+        if user in guild.members:
+            guilds.append(guild)
+    return guilds
+def get_channels(guild):
+    return guild.voice_channels
+
+
+
+bot.run(TOKEN)
+
+
+
+
+
